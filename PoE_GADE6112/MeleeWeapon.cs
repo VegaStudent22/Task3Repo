35262faadcbd090c2e3c05823df0cff535f15c2c
Luke <lukejjones101@gmail.com>
2021-11-20 13:08:09 +0200

    MeleeWeapon subclass created

---
+++ PoE_GADE6112/MeleeWeapon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoE_GADE6112
+{
+    public class MeleeWeapon : Weapon
+    {
+        public enum MeleeWeaponTypes
+        {
+            DAGGER, LONGSWORD
+        }
+
+        public MeleeWeaponTypes meleeWeaponTypes { get; set; }
+
+        public MeleeWeapon(MeleeWeaponTypes meleeTypes, int x, int y, TileType tileType) : base(x, y)
+        {
+            meleeWeaponTypes = meleeTypes;
+        }
+
+        public override int Range()
+        {
+            return 1;
+        }
+
+        if(meleeWeaponTypes == MeleeWeaponTypes.DAGGER)
+
+
+    }
+}
---
+++ PoE_GADE6112/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoE_GADE6112
+{
+    public class Weapon : Item
+    {
+        private int damage;
+        protected int Damage {get { return this.damage; } set { damage = value; } } //protected accessor for the Damage value
+
+        public virtual int Range();
+
+        private durability;
+        protected int Durability {get { return this.durability; } set { durability = value; } } //protected accessor for the Durability value
+
+        private cost;
+        protected int Cost {get { return this.cost; } set { cost = value; } } //protected accessor for the Cost value
+
+        private weaponType;
+        protected string WeaponType {get { return this.weaponType; } set { weaponType = value; } } //protected accessor for the WeaponType value
+        
+        public Weapon(int x, int y): base(x, y, TileType.WEAPON)
+        {
+            
+        }
+    }
+}
